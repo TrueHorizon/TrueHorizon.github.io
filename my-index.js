16c8bcf4aafc5dd2b6a5dda456634f54a62a8ee0
TrueHorizon <cps124@gmail.com>
2019-07-05 12:04:05 -0400

    Create my-index.js
    Initial checking of the table on the index.html page.

---
+++ my-index.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  function remove() {
+    $(".red").click(function(){
+      $(this).closest("tr").remove();
+    });
+  }
+  remove();
+  $(".add-form").on('submit', function(e){
+    e.preventDefault();
+    if($("input[name=date]").val(), $("input[name=date]").val(), $("input[name=date]").val() == "") {
+      $(".alert").addClass("active");
+      $(".remove").click(function(){
+        $(".alert").removeClass("active");
+      });
+    } else {
+      var date = $("input[name=date]").val();
+      var event = $("input[name=event]").val();
+      $(".first-tr").after('<tr><td></td><td>'+date+'</td><td>'+event+'</td><td class="static"><span class="button red"><i class="glyphicon glyphicon-remove"></i></span></td></tr>');
+      remove();
+      $("input[name=date], input[name=event]").val("");   
+    }
+  });
+});
